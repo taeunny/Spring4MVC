05f065c82e65410c36e10106d281ed39b5a3b4e6
TaeEun Park <ubub0322@gmail.com>
2023-06-28 11:39:07 +0900

    Tiles3 실행 확인

--- src/main/java/taeeun/hello/spring4/controller/IndexController.java
+++ src/main/java/taeeun/hello/spring4/controller/IndexController.java
@@ -24,7 +24,8 @@ public class IndexController {
             // 로거 출력
             logger.info("Hello, world!! from Logger!");
 
-            return "index";
+            //return "index";   // jsp view resolver
+            return "index.tiles";
     }
 
 }
