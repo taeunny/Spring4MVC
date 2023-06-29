323c92c042a254d24842e1e0026a91dc1e425fa7
TaeEun Park <ubub0322@gmail.com>
2023-06-29 15:16:38 +0900

    MemberDAO loginMember 단위 테스트

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -1,5 +1,7 @@
 package dao;
 
+import taeeun.hello.spring4.dao.MemberDAO;
+import taeeun.hello.spring4.model.Member;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,31 +9,36 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-import taeeun.hello.spring4.dao.MemberDAO;
-import taeeun.hello.spring4.model.Member;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:spring/servlet-context.xml",
-                        "classpath:spring/root-context.xml" })
+        "classpath:spring/root-context.xml"})
 @WebAppConfiguration
-
 public class MemberDAOUnitTest {
 
-    // 테스트할 대상 객체를 주입받음
+    // 테스트 할 대상 객체를 주입받음
     @Autowired MemberDAO mdao;
 
     @Test
-    @Transactional          // 데이터 입력후 자동 삭제
+    @Transactional  // 데이터 입력 후 자동 삭제
     public void insertMember() throws Exception {
-        Member m = new Member(null, "abc123a","987xyz",
-                            "abc123","abc123@987xyz.co.kr",null);
+        Member m = new Member(null, "abc123a","987xyz","빵빵이","abc123@987xyz.co.kr",null);
+        // assertEquals(테스트메서드, 검증값)
+        assertEquals(mdao.insertMember(m), 1);
+    }
+    @Test
+    public void loginMember() throws Exception {
 
-        // assertEquals(테스트 메서드, 검증값)
-        assertEquals(mdao.insertMember(m),1);
+        Member m = new Member();
+        m.setUserid("abc123");
+        m.setPasswd("987xyz");
+
+        // assertEquals(테스트메서드, 검증값)
+        //System.out.println(mdao.loginMember(m));
+        assertNotNull(mdao.loginMember(m));
 
     }
-
-
 }
