b79c50fa978c475be255dcb2c1a7283fc825cf3e
TaeEun Park <ubub0322@gmail.com>
2023-06-29 17:36:39 +0900

    세션객체 존재 여부에 따라 myinfo/login 페이지 표시

--- src/main/java/taeeun/hello/spring4/controller/MemberController.java
+++ src/main/java/taeeun/hello/spring4/controller/MemberController.java
@@ -67,6 +67,11 @@ public class MemberController {
     @RequestMapping("/member/myinfo")
     public String myinfo(Model m , HttpSession sess) {
         logger.info("member/myinfo 호출!");
+
+        // 세션객체가 없을 경우 로그인 페이지로 이동
+        if(sess.getAttribute("member") ==null)
+            return "redirect:/member/login";
+
         String userid =
                 ((Member)sess.getAttribute("member")).getUserid();      // 다운캐스팅
 
@@ -74,5 +79,12 @@ public class MemberController {
 
         return "member/myinfo.tiles";
     }
+    @RequestMapping("/member/logout")
+    public String logout(Model m , HttpSession sess) {
+        logger.info("member/logout 호출!");
 
+        sess.invalidate();      // 세션 객체 제거
+
+        return "redirect:/";
+    }
 }
