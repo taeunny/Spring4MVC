daa04ff88b8e85ab142df5ff1103c5a48ff256b9
TaeEun Park <ubub0322@gmail.com>
2023-06-29 16:50:03 +0900

    MemberDAO selectOneMember 단위 테스트

--- src/test/java/dao/MemberDAOUnitTest.java
+++ src/test/java/dao/MemberDAOUnitTest.java
@@ -41,4 +41,13 @@ public class MemberDAOUnitTest {
         assertNotNull(mdao.loginMember(m));
 
     }
+    @Test
+    public void selectOneMember() throws Exception {
+
+        String userid = "abc123";
+
+        System.out.println(mdao.selectOneMember(userid));
+        assertNotNull(mdao.selectOneMember(userid));
+    }
+
 }
