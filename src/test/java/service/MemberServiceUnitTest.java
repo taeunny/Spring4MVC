a6252c96d9195b5bbfaa8a2ec767e3fe22665db1
TaeEun Park <ubub0322@gmail.com>
2023-06-29 12:09:10 +0900

    MemberService 단위테스트 추가

---
+++ src/test/java/service/MemberServiceUnitTest.java
@@ -0,0 +1,37 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import taeeun.hello.spring4.dao.MemberDAO;
+import taeeun.hello.spring4.model.Member;
+import taeeun.hello.spring4.service.MemberService;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+                        "classpath:spring/root-context.xml" })
+@WebAppConfiguration
+
+public class MemberServiceUnitTest {
+
+    @Autowired MemberService msrv;
+
+    @Test
+    @Transactional
+    public void saveMember() throws Exception {
+        Member m = new Member(null, "abc123a","987xyz",
+                            "abc123","abc123@987xyz.co.kr",null);
+
+        // assertEquals(테스트 메서드, 검증값)
+        assertEquals(msrv.saveMember(m),true);
+
+    }
+
+
+}
