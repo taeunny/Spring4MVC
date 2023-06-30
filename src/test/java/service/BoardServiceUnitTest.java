441617d00c157d452f21be78ae4b7f4d89bb9737
TaeEun Park <ubub0322@gmail.com>
2023-06-30 16:54:00 +0900

    BoardService : readBoard 단위 테스트

---
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -0,0 +1,37 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import taeeun.hello.spring4.model.Board;
+import taeeun.hello.spring4.model.Member;
+import taeeun.hello.spring4.service.BoardService;
+import taeeun.hello.spring4.service.MemberService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+                        "classpath:spring/root-context.xml" })
+@WebAppConfiguration
+
+public class BoardServiceUnitTest {
+
+    @Autowired BoardService bsrv;
+
+    @Test
+    public void readBoard() throws Exception {
+        int cpage = 1;
+        List<Board> results = bsrv.readBoard(cpage);
+
+        assertEquals(results.size(),15);
+        System.out.println(results);
+    }
+}
