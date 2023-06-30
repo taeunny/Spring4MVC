f79038ffd666c592a3522b592007c04141da92db
TaeEun Park <ubub0322@gmail.com>
2023-06-30 16:54:59 +0900

    BoardController : list 구현

--- src/main/java/taeeun/hello/spring4/controller/BoardController.java
+++ src/main/java/taeeun/hello/spring4/controller/BoardController.java
@@ -2,34 +2,40 @@ package taeeun.hello.spring4.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import taeeun.hello.spring4.service.BoardService;
 
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired BoardService bsrv;
+
     private Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list(Model m) {
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
 
+        m.addAttribute("boards",bsrv.readBoard(cpg));
 
         return "board/list.tiles";
     }
 
     @GetMapping("/write")
-    public String write(Model m) {
+    public String write() {
 
 
         return "board/write.tiles";
     }
 
     @GetMapping("/view")
-    public String view(Model m) {
+    public String view() {
 
 
         return "board/view.tiles";
