6c48befbf03386987e757647c3f025582eddd7e6
TaeEun Park <ubub0322@gmail.com>
2023-07-03 11:12:59 +0900

    boardService : readOneBoard 단위 테스트

--- src/test/java/service/BoardServiceUnitTest.java
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -34,4 +34,15 @@ public class BoardServiceUnitTest {
         assertEquals(results.size(),15);
         System.out.println(results);
     }
+    @Test
+    public void readOneBoard() throws Exception {
+        String bno = "200";
+        Board result = bsrv.readOneBoard(bno);
+
+        assertNotNull(result);;
+        System.out.println(result);
+    }
+
+
 }
+
