e0facddf934ddd2af39c1c823844a119503c361b
TaeEun Park <ubub0322@gmail.com>
2023-07-03 14:51:53 +0900

    boardDAO : 조회수 증가, insertBoard 구현

--- src/main/java/taeeun/hello/spring4/dao/BoardDAO.java
+++ src/main/java/taeeun/hello/spring4/dao/BoardDAO.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface BoardDAO {
     List<Board> selectBoard(int snum);
-
     Board selectOneBoard(String bno);
+
+    // 클래스 사용: 여러개의 변수를 숨겨서 보낼 수 있는 장점(캡슐화)
+    int insertBoard(Board bd);
 }
--- src/main/java/taeeun/hello/spring4/dao/BoardDAOImpl.java
+++ src/main/java/taeeun/hello/spring4/dao/BoardDAOImpl.java
@@ -17,6 +17,8 @@ public class BoardDAOImpl implements BoardDAO {
 
     @Value("#{sql['selectBoard']}") private String selectSQL;
     @Value("#{sql['selectOneBoard']}") private String selectOneSQL;
+    @Value("#{sql['viewCountBoard']}") private String viewCountSQL;
+    @Value("#{sql['insertBoard']}") private String insertSQL;
 
 
     @Autowired JdbcTemplate jdbcTemplate;
@@ -33,7 +35,20 @@ public class BoardDAOImpl implements BoardDAO {
         Object[] params = new Object[] {bno};
 
         RowMapper<Board> mapper = new SelectOneMapper();
+
+        // 조회수 증가
+        jdbcTemplate.update(viewCountSQL, params);
+
+
         return jdbcTemplate.queryForObject(selectOneSQL, params, mapper);
+    }
+
+    @Override
+    public int insertBoard(Board bd) {
+        Object[] params = new Object[] {
+              bd.getTitle(), bd.getUserid(), bd.getContents()
+        };
+        return jdbcTemplate.update(insertSQL, params);
     }
 
     private class SelectBoardMapper implements RowMapper<Board> {
