acfd1cba10abe72484fffa3e2df2b9e52d323dbc
TaeEun Park <ubub0322@gmail.com>
2023-07-03 11:11:01 +0900

    boardDAO : selectOneBoard 구현

--- src/main/java/taeeun/hello/spring4/dao/BoardDAO.java
+++ src/main/java/taeeun/hello/spring4/dao/BoardDAO.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface BoardDAO {
     List<Board> selectBoard(int snum);
+
+    Board selectOneBoard(String bno);
 }
--- src/main/java/taeeun/hello/spring4/dao/BoardDAOImpl.java
+++ src/main/java/taeeun/hello/spring4/dao/BoardDAOImpl.java
@@ -1,43 +1,64 @@
 package taeeun.hello.spring4.dao;
 
+import taeeun.hello.spring4.model.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-import taeeun.hello.spring4.model.Board;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+
 @Repository("bdao")
-public class BoardDAOImpl implements BoardDAO{
+public class BoardDAOImpl implements BoardDAO {
 
     @Value("#{sql['selectBoard']}") private String selectSQL;
+    @Value("#{sql['selectOneBoard']}") private String selectOneSQL;
+
 
     @Autowired JdbcTemplate jdbcTemplate;
-
     @Override
     public List<Board> selectBoard(int snum) {
-        Object[] params = new Object[] { snum };
-        RowMapper<Board> mapper = new SelectMapper();
+        Object[] params = new Object[] {snum};
+        RowMapper<Board> mapper = new SelectBoardMapper();
 
-        return jdbcTemplate.query(selectSQL, params, mapper);
+        return jdbcTemplate.query(selectSQL,params,mapper);
     }
 
-    private class SelectMapper implements RowMapper<Board> {
+    @Override
+    public Board selectOneBoard(String bno) {
+        Object[] params = new Object[] {bno};
 
+        RowMapper<Board> mapper = new SelectOneMapper();
+        return jdbcTemplate.queryForObject(selectOneSQL, params, mapper);
+    }
+
+    private class SelectBoardMapper implements RowMapper<Board> {
 
         @Override
         public Board mapRow(ResultSet rs, int num) throws SQLException {
-            // sql에서 불러온 컬럼의 순서에 맞춰서 쓴 번호.
             Board bd = new Board(rs.getString(1),
-                    rs.getString(2), rs.getString(3),
-                    rs.getString(4), rs.getString(5),
+                    rs.getString(2),rs.getString(3),
+                    rs.getString(4),rs.getString(5),
                     null);
 
             return bd;
         }
     }
-}
+    private class SelectOneMapper implements RowMapper<Board> {
+
+        @Override
+        public Board mapRow(ResultSet rs, int num) throws SQLException {
+            Board bd = new Board(rs.getString(1),
+                    rs.getString(2),rs.getString(3),
+                    rs.getString(4),rs.getString(5),
+                    rs.getString(6));
+
+            return bd;
+        }
+    }  //selectOneMapper
+
+} // class
